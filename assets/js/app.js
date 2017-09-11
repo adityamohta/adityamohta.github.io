77ea9887b1079802b7f0f9f24bc93e7dce2843b3
aditya <adityamohta@bitbucket.org>
2017-09-11 09:42:24 +0530

    Updates:
    - Updated File Structure.
    - Added 404 page.

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by aditya on 9/11/17.
+ */
+
+// ParticlesJS Config.
+particlesJS.load("particles-js", "particles.json", function() {
+    console.log('callback - particles.js config loaded');
+});
