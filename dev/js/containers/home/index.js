1654b43dde7021c06b31f2c664dac90ff28fdd88
aditya <adityamohta@bitbucket.org>
2018-02-17 14:40:58 +0530

    Fixed left panel

--- dev/js/containers/home/index.js
+++ dev/js/containers/home/index.js
@@ -14,29 +14,31 @@ require('../utils/background.sass');
 
 const Home = () => (
     <React.Fragment>
-    <Particles className="background-particle-js" params={values}/>
-    <div className="home">
-        <div className="panel-center">
-            <Banner greeting="Hello Visitor! :D" title="Kumar Aditya Mohta"/>
-            <UserIcon />
-            <UserAbout/>
+        <Particles className="background-particle-js" params={values}/>
+        <div className="home">
+            <div className="panel-center">
+                <Banner greeting="Hello Visitor! :D" title="Kumar Aditya Mohta"/>
+                <UserIcon />
+                <UserAbout/>
+            </div>
+            <div className="panel-left">
+                <table cellPadding="0" cellSpacing="0" border="0">
+                    <tr><td><Buttons.HackerEarth /></td></tr>
+                    <tr><td><Buttons.LinkedIn /></td></tr>
+                    <tr><td><Buttons.Github /></td></tr>
+                    <tr><td><Buttons.AmcatReport /></td></tr>
+                    <tr><td><Buttons.Resume /></td></tr>
+                    <tr><td><Buttons.ResumeDownload /></td></tr>
+                </table>
+            </div>
+            <div className="panel-right">
+                <h5 className="text-center">PROJECTS</h5>
+                <ProjectCard
+                    to="/ace"
+                    title="ACE"
+                    description="ACE can be used to create shared boards of lists of cards with notes. Built with React"/>
+            </div>
         </div>
-        <div className="panel-left">
-            <Buttons.HackerEarth />
-            <Buttons.LinkedIn />
-            <Buttons.Github />
-            <Buttons.AmcatReport />
-            <Buttons.Resume />
-            <Buttons.ResumeDownload />
-        </div>
-        <div className="panel-right">
-            <h5 className="text-center">PROJECTS</h5>
-            <ProjectCard
-                to="/ace"
-                title="ACE"
-                description="ACE can be used to create shared boards of lists of cards with notes. Built with React"/>
-        </div>
-    </div>
     </React.Fragment>
 );
 
